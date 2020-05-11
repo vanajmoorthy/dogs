9d1e2410998af6a4fe7341ed6221054e59c44f28
vanajmoorthy <vanajmoorthy@gmail.com>
2020-05-11 12:42:30 +0530

    change input button text, remove focus outline; redo list order stuff ykwim

--- populate-select.js
+++ populate-select.js
@@ -12,7 +12,7 @@ function populate(data) {
 	for (let element in data.message) {
 		if (data.message[element].length > 0) {
 			for (let i = 0; i < data.message[element].length; i++) {
-				breeds.innerHTML += `<option value=${data.message[element][i]}-${element}>${data.message[element][i]} ${element}</option>`;
+				breeds.innerHTML += `<option value=${data.message[element][i]}-${element}>${element} (${data.message[element][i]})</option>`;
 			}
 		} else {
 			breeds.innerHTML +=
